3b1de604ee0f07ded4a3357ebffcf86fe0b75dd4
trakieca <emil_vaklinov@yahoo.com>
2018-08-22 00:40:11 +0100

    Restful for employee done

--- src/main/java/controllers/EmployeesController.java
+++ src/main/java/controllers/EmployeesController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import db.DBHelper;
 import db.Seeds;
+import models.Department;
 import models.Employee;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class EmployeesController {
 
@@ -34,5 +36,91 @@ public class EmployeesController {
 
         }, new VelocityTemplateEngine());
 
+        //        # new
+//        get '/className/new'
+
+        get("/employees/new", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/employees/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+//     # create
+//        post '/className'
+
+        post("/employees", (req, res) -> {
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+            Employee employee = new Employee(firstName, lastName, salary, department) {
+            };
+            DBHelper.save(employee);
+            res.redirect("/employees");
+            return null;
+        }, new VelocityTemplateEngine());
+
+
+        // # edit
+//        get '/className/:id/edit'
+
+        get("/employees/:id/edit", (req, res) -> {
+
+                    Map<String, Object> model = new HashMap<>();
+                    model.put("template", "templates/employees/edit.vtl");
+
+                    int employee = Integer.parseInt(req.params(":id"));
+                    Employee employeeId = DBHelper.find(employee, Employee.class);
+                    model.put("employee", employee);
+
+                    List<Department> departments = DBHelper.getAll(Department.class);
+                    model.put("departments", departments);
+
+                    return new ModelAndView(model, "templates/layout.vtl");
+
+                }, new VelocityTemplateEngine()
+        );
+
+
+        //        # update
+//        post '/className/:id'
+
+
+        post("/employees/:id", (req, res) -> {
+
+                    int employeeId = Integer.parseInt(req.params(":id"));
+                    Employee employee = DBHelper.find(employeeId, Employee.class);
+
+                    String firstName = req.queryParams("firstName");
+                    String lastName = req.queryParams("lastName");
+                    int salary = Integer.parseInt(req.queryParams("salary"));
+                    int departmentId = Integer.parseInt(req.queryParams("departmentId"));
+                    Department department = DBHelper.find(departmentId, Department.class);
+
+                    //update with new values of properties
+                    employee.setFirstName(firstName);
+                    employee.setLastName(lastName);
+                    employee.setSalary(salary);
+                    employee.setDepartment(department);
+
+                    //update in the db and redirect to the managers page
+                    DBHelper.save(employee);
+                    res.redirect("/employees");
+                    return null;
+                }
+        );
+
+        //        # destroy
+//        get '/className/:id/delete'
+
+        post("/employees/:id/delete", (req, res) -> {
+            int employeeId = Integer.parseInt(req.params(":id"));
+            Employee employee = DBHelper.find(employeeId, Employee.class);
+            DBHelper.delete(employee);
+            res.redirect("/employees");
+            return null;
+        });
     }
 }
--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
+import models.Manager;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 
 public class EngineersController {
@@ -29,5 +32,92 @@ public class EngineersController {
 
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        //        # new
+//        get '/className/new'
+
+        get("/engineers/new", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/engineers/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+//     # create
+//        post '/className'
+
+        post("/engineers", (req, res) -> {
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+            Engineer engineer = new Engineer(firstName, lastName, salary, department);
+            DBHelper.save(engineer);
+            res.redirect("/engineers");
+            return null;
+        }, new VelocityTemplateEngine());
+
+
+        // # edit
+//        get '/className/:id/edit'
+
+        get("/engineers/:id/edit", (req, res) -> {
+
+                    Map<String, Object> model = new HashMap<>();
+                    model.put("template", "templates/engineers/edit.vtl");
+
+                    int engineer = Integer.parseInt(req.params(":id"));
+                    Engineer engineerId = DBHelper.find(engineer, Engineer.class);
+                    model.put("engineer", engineer);
+
+                    List<Department> departments = DBHelper.getAll(Department.class);
+                    model.put("departments", departments);
+
+                    return new ModelAndView(model, "templates/layout.vtl");
+
+                }, new VelocityTemplateEngine()
+        );
+
+
+        //        # update
+//        post '/className/:id'
+
+
+        post("/engineers/:id", (req, res) -> {
+
+                    int engineerId = Integer.parseInt(req.params(":id"));
+                    Engineer engineer = DBHelper.find(engineerId, Engineer.class);
+
+                    String firstName = req.queryParams("firstName");
+                    String lastName = req.queryParams("lastName");
+                    int salary = Integer.parseInt(req.queryParams("salary"));
+                    int departmentId = Integer.parseInt(req.queryParams("departmentId"));
+                    Department department = DBHelper.find(departmentId, Department.class);
+
+                    //update with new values of properties
+                    engineer.setFirstName(firstName);
+                    engineer.setLastName(lastName);
+                    engineer.setSalary(salary);
+                    engineer.setDepartment(department);
+
+                    //update in the db and redirect to the managers page
+                    DBHelper.save(engineer);
+                    res.redirect("/engineers");
+                    return null;
+                }
+        );
+
+        //        # destroy
+//        get '/className/:id/delete'
+
+        post("/engineers/:id/delete", (req, res) -> {
+            int engineerId = Integer.parseInt(req.params(":id"));
+            Engineer engineer = DBHelper.find(engineerId, Engineer.class);
+            DBHelper.delete(engineer);
+            res.redirect("/engineers");
+            return null;
+        });
     }
-}
+    }
+
