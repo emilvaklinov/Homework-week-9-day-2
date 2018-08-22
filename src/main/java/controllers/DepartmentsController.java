f9c9b6fc3af904271dc8643f35e4d12e0f744b42
trakieca <emil_vaklinov@yahoo.com>
2018-08-22 01:29:41 +0100

    Updated

--- src/main/java/controllers/DepartmentsController.java
+++ src/main/java/controllers/DepartmentsController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class DepartmentsController {
     public DepartmentsController(){
@@ -24,6 +25,33 @@ public class DepartmentsController {
             model.put("departments", departments);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+
+        get("/departments/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/departments/index.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/departments", (req, res) -> {
+            String title = req.queryParams("title");
+
+            Department addingDepartment = new Department(title);
+            DBHelper.save(addingDepartment);
+
+            res.redirect("/departments");
+            return null;
+        }, new VelocityTemplateEngine());
+
+//        get("/departments/:id", (req, res) -> {
+//            int departmentId = Integer.parseInt(req.queryParams("id"));
+//            Department foundDepartment = DBHelper.find(departmentId, Department.class);
+//            model.put("template", "templates/departments/index.vtl");
+//            model.put("departments", departments);
+//            return new ModelAndView(model, "templates/layout.vtl");
+//        }, new VelocityTemplateEngine());
+
+
     }
 
 
